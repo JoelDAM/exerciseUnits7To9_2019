79acd11f94b557b03ade23c0d820e932cb685bb1
Joel <martinezsanchezjoel1@gmail.com>
2019-10-31 18:52:23 +0100

    Few comments

--- CheckFinalMark.cpp
+++ CheckFinalMark.cpp
@@ -1,3 +1,13 @@
+// Joel Martinez
+/*
+ * Create a program called CheckFinalMark that asks the user to enter 10 integer, 
+ * positive numbers. At the end of the process, 
+ * the program must tell how many increments and decrements have taken place. 
+ * For instance, if the user enters this sequence: 3, 4, 2, 8, 3, 3, 7, 1, 6, 6,
+ * the program must output that there has been 4 increments 
+ * (3 to 4, 2 to 8, 3 to 7 and 1 to 6), and 3 decrements 
+ * (4 to 2, 8 to 3 and 7 to 1).
+ */
 #include <iostream>
 using namespace std;
 int main()
--- TextWithoutA.cs
+++ TextWithoutA.cs
@@ -1,4 +1,11 @@
 //Joel Martinez
+/*
+ * Create a program called TextWithoutA that asks the user to enter a 
+ * text iteratively until he/she types a text with no 'a' or 'A' symbols in it. 
+ * For instance, if the user types "programming", or "Author" 
+ * then the program must keep on asking him/her to type another text. 
+ * If he/she types "believe", then the program must finish.
+ */
 using System;
 
 class TextWithoutA
